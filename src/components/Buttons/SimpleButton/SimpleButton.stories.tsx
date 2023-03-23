609151b856a224e297a8978df48aca555435fb22
mojsx <mojsx.dev@gmail.com>
2023-03-23 17:09:51 +0100

    story: add Secondary, Danger, Success stories.

--- src/components/Buttons/SimpleButton/SimpleButton.stories.tsx
+++ src/components/Buttons/SimpleButton/SimpleButton.stories.tsx
@@ -10,6 +10,30 @@ export default {
 
 export const Primary: Story<ButtonProps> = (args) => <SimpleButton {...args} />;
 Primary.args = {
-    label: "Button",
+    label: "Primary Button",
     type: "button",
+    variant: "primary",
+};
+
+export const Secondary: Story<ButtonProps> = (args) => (
+    <SimpleButton {...args} />
+);
+Secondary.args = {
+    label: "Secondary Button",
+    type: "button",
+    variant: "secondary",
+};
+
+export const Danger: Story<ButtonProps> = (args) => <SimpleButton {...args} />;
+Danger.args = {
+    label: "Danger Button",
+    type: "button",
+    variant: "danger",
+};
+
+export const Success: Story<ButtonProps> = (args) => <SimpleButton {...args} />;
+Success.args = {
+    label: "Success Button",
+    type: "button",
+    variant: "success",
 };
