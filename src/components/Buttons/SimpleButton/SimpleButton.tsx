edb4988edf2c080a5bbd82a15e3a4142f9e891d1
mojsx <mojsx.dev@gmail.com>
2023-03-23 13:42:08 +0100

    component: add Wrapper.

--- src/components/Buttons/SimpleButton/SimpleButton.tsx
+++ src/components/Buttons/SimpleButton/SimpleButton.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Wrapper } from "../../../utils";
 import Button from "types/Button";
+import "./styles.scss";
 
 const SimpleButton = (props: Button) => {
     const { label, type } = props;
-    return <button type={type}>{label}</button>;
+    return (
+        <Wrapper>
+            <button className="btn" type={type}>
+                {label}
+            </button>
+        </Wrapper>
+    );
 };
 
 export default SimpleButton;
